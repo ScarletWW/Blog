521efa9666b35fe59b1c7589b341050b4e63bffd
wanght <h519481156@hotmail.com>
2017-05-10 15:18:08 +0800

    new view

--- microblog-0.1/app/views.py
+++ microblog-0.1/app/views.py
@@ -1,6 +1,11 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    #return "Hello, World!"
+    user = {'nickname': 'Mike'}
+    return render_template("index.html",
+        title = 'Home',
+        user = user)
