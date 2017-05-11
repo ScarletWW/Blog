c55c2fdbe83d935d2772cf4f7e4c6ef7c4a27464
wanght <h519481156@hotmail.com>
2017-05-11 08:39:17 +0800

    blog2.0

--- microblog-0.1/app/views.py
+++ microblog-0.1/app/views.py
@@ -6,6 +6,17 @@ from flask import render_template
 def index():
     #return "Hello, World!"
     user = {'nickname': 'Mike'}
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
     return render_template("index.html",
         title = 'Home',
-        user = user)
+        user = user,
+        posts = posts)
